f657a1d1abf7dbaf586f85ad4093a64641c09812
unknown <dgstirling@yahoo.co.uk>
2021-03-15 18:27:44 +0000

    changed a file

--- NearestNow/AngularDynamicFormFieldGeneration/create.blade.php
+++ NearestNow/AngularDynamicFormFieldGeneration/create.blade.php
@@ -42,6 +42,7 @@ Create an Advert
          <option value="international">International</option>
          <option value="UK">UK</option>
       </select>
+      <!-- ==================== international ====================== -->  
       <div ng-show="user.region == 'international'">
          <h3>%%user.region%% locations</h3>
          <select name="internationalCountryID" ng-model="user.selectedcountry" 
@@ -52,17 +53,20 @@ Create an Advert
          <select ng-selected="'5'" name="internationalCityID" ng-model="user.selectLocationInt" ng-options="k as v for (k , v) in cities[user.selectedcountry]['cities']">
          </select> 	   
       </div>
+      <!-- ==================== UK ====================== -->  
       <div ng-show="user.region == 'UK'">
          <h3>%%user.region%% locations</h3>
          <select name="UKCItyID" ng-model="user.selectLocationUK" ng-options="k as v for (k , v) in cities['23']['cities']">
          </select> 
       </div>
+      <!-- ==================== searchType ====================== -->  
       <div ng-show="user.selectLocationInt || user.selectLocationUK">
          <h3 ng-show="user.region == 'UK'">You have selected%%cities['23']['cities'][user.selectLocation]%%</h3>
          <h3 ng-show="user.region != 'UK'">You have selected %%cities[user.selectedcountry]['cities'][user.selectLocation]%%</h3>
          <h4>Select a search type</h4>
          <select name="searchType" ng-model="user.searchTypeSelected" ng-options="k as v for (k , v) in searchType"> </select>
       </div>
+      <!-- ==================== business finder ====================== -->	
       <div ng-if="user.searchTypeSelected == '1'">
          <div ng-show="user.searchTypeSelected">
             <h3>You have selected %%searchType[user.searchTypeSelected]%%</h3>
@@ -78,6 +82,7 @@ Create an Advert
             </div>
          </div>
       </div>
+      <!-- ==================== community finder ====================== -->
       <div ng-if="user.searchTypeSelected == '2'">
          <div ng-show="user.searchTypeSelected">
             <h3>You have selected %%searchType[user.searchTypeSelected]%%</h3>
@@ -85,6 +90,7 @@ Create an Advert
             <select name="comParent" ng-model="user.comParent" ng-options="k as v for (k , v) in communityFinder"></select>		
          </div>
       </div>
+      <!-- ==================== marketplace ====================== --> 
       <div ng-if="user.searchTypeSelected == '3'">
          <div ng-show="user.searchTypeSelected">
             <h3>You have selected %%searchType[user.searchTypeSelected]%%</h3>
@@ -93,118 +99,7 @@ Create an Advert
             </select>  		
          </div>
       </div>
-      <!-- ###### create data structures for Angular code above ###### -->     
    </div>
-   <?php
-      $cities = array(); 
-      ?>
-   @foreach ($internationalcities as $city)	
-   <?php 	
-      $cityID      = $city->id;
-      $cityName    = $city->city_name;
-      $countryID   = $city->country_id + 0;
-      $countryName = $city->country_name;	
-      
-      if($city->country_id == 23){
-		  //UK
-		  // Rest of the world
-		  $cities[23]['cities'][$cityID] = $cityName;			
-		  $cities[23]['countryName']     = $countryName;			
-      }else{
-		  // Rest of the world
-		  $cities[$countryID]['cities'][$cityID] = $cityName;			
-		  $cities[$countryID]['countryName']     = $countryName;				
-      }
-      ?>
-   @endforeach	 
-   <?php 
-      $citiesJsonEncoded = json_encode((object)$cities);			
-      $searchtypesArr = array(); 
-      ?>
-   @foreach ($searchtypes as $type)	
-   <?php 		
-      $searchtypesArr[$type->id] = $type->searchtypes_description;		
-      ?>
-   @endforeach	 
-   <?php 	
-      $searchtypesJsonEncoded = json_encode((object)$searchtypesArr);	
-      $businessFinderArr = array();
-      $communityFinderArr = array();
-      ?>
-   @foreach ($businessAndCommunity as $category)	
-   <?php 
-      $parentTitle = $category->searchparentcategory_title;
-      $parentName  = $category->searchparentcategory_name;	
-      $parentID    = $category->parentID;
-      $childID     = $category->childID;
-      $childName   = $category->searchchildcategory_name;	
-      
-      if($category->searchtype_id == 1){	
-		  $businessFinderArr[$parentID]['parent']['parentName']  = $parentName;	
-		  $businessFinderArr[$parentID]['parent']['parentTitle'] = $parentTitle;				
-		  $businessFinderArr[$parentID]['child'][$childID]	   = $childName;				
-      }else if($category->searchtype_id == 2){	
-		  $communityFinderArr[$parentID] = $parentName;
-      }			
-      ?>
-   @endforeach	 
-   <?php 
-      $businessFinderJsonEncoded  = json_encode((object)$businessFinderArr);	
-      $communityFinderJsonEncoded = json_encode((object)$communityFinderArr);	
-      $marketplaceArr = array();
-      ?>
-   @foreach ($marketplaces as $category)	
-   <?php 
-      $marketID = $category->id;
-      $marketplaceArr[$marketID] = $category->market_category;			
-      ?>
-   @endforeach	 
-   <?php 
-      $marketplaceJsonEncoded = json_encode($marketplaceArr);
-      ?>
-   <script>
-      var app = angular.module('myApp', []);
-      
-      app.config(function($interpolateProvider) {
-		  $interpolateProvider.startSymbol('%%');
-		  $interpolateProvider.endSymbol('%%');
-      });
-      
-      app.controller('myCtrl', function($scope) {
-		  $scope.cities          = <?php echo $citiesJsonEncoded ?>;	
-		  $scope.searchType      = <?php echo $searchtypesJsonEncoded ?>;		
-		  $scope.businessFinder  = <?php echo $businessFinderJsonEncoded ?>;
-		  $scope.communityFinder = <?php echo $communityFinderJsonEncoded ?>;
-		  $scope.marketplace     = <?php echo $marketplaceJsonEncoded;?>;
-		  
-		  var oldInternationalCountryID = ("{{ old('internationalCountryID') }}".split(":"))[1];
-		  var oldInternationalCityID    = ("{{ old('internationalCityID') }}".split(":"))[1];
-		  var oldUKCItyID = ("{{ old('UKCItyID') }}".split(":"))[1];
-		  var oldSearchType = ("{{ old('searchType') }}".split(":"))[1];
-		  var oldBfParent = ("{{ old('bfParent') }}".split(":"))[1];
-		  var oldBfChild  = ("{{ old('bfChild') }}".split(":"))[1];
-		  var oldComParent  = ("{{ old('comParent') }}".split(":"))[1];
-		  var oldMarketParent  = ("{{ old('marketParent') }}".split(":"))[1];
-		  
-		  $scope.master = {
-			  region:"{{ old('region') }}", 
-			  selectedcountry:oldInternationalCountryID,
-			  selectLocationInt:oldInternationalCityID,
-			  selectLocationUK:oldUKCItyID,
-			  searchTypeSelected:oldSearchType,
-			  bfParent:oldBfParent,
-			  bfChild:oldBfChild,
-			  comParent:oldComParent,
-			  oldMarketParent:oldMarketParent
-		  };
-		  
-		  $scope.reset = function() {
-			  $scope.user = angular.copy($scope.master);
-		  };
-		  
-		  $scope.reset();
-      });
-   </script>
    <br /><br />
    <div class="form-group">
       <textarea name='body' class="form-control">{{ old('body') }}</textarea>
@@ -212,4 +107,119 @@ Create an Advert
    <input type="submit" name='publish' class="btn btn-success" value="Publish" />
    <input type="submit" name='save' class="btn btn-default" value="Save Draft" />
 </form>
+
+<!-- ################### create data for Angular menus above ###################  -->
+
+<?php $cities = array(); ?>
+@foreach ($internationalcities as $city)	
+<?php 	
+   $cityID      = $city->id;
+   $cityName    = $city->city_name;
+   $countryID   = $city->country_id + 0;
+   $countryName = $city->country_name;	
+   
+   if($city->country_id == 23){
+	   //UK
+	   // Rest of the world
+	   $cities[23]['cities'][$cityID] = $cityName;			
+	   $cities[23]['countryName']     = $countryName;			
+   }else{
+	   // Rest of the world
+	   $cities[$countryID]['cities'][$cityID] = $cityName;			
+	   $cities[$countryID]['countryName']     = $countryName;				
+   }
+   ?>
+@endforeach	 
+<?php 
+   $citiesJsonEncoded = json_encode((object)$cities);			
+   //============================
+   $searchtypesArr = array(); 
+   ?>
+@foreach ($searchtypes as $type)	
+<?php 		
+   $searchtypesArr[$type->id] = $type->searchtypes_description;		
+   ?>
+@endforeach	 
+<?php 	
+   $searchtypesJsonEncoded = json_encode((object)$searchtypesArr);	
+   //============================
+   $businessFinderArr = array();
+   $communityFinderArr = array();
+   ?>
+@foreach ($businessAndCommunity as $category)	
+<?php 
+   $parentTitle = $category->searchparentcategory_title;
+   $parentName  = $category->searchparentcategory_name;	
+   $parentID    = $category->parentID;
+   $childID     = $category->childID;
+   $childName   = $category->searchchildcategory_name;	
+   
+   if($category->searchtype_id == 1){	
+	   $businessFinderArr[$parentID]['parent']['parentName']  = $parentName;	
+	   $businessFinderArr[$parentID]['parent']['parentTitle'] = $parentTitle;				
+	   $businessFinderArr[$parentID]['child'][$childID]	   = $childName;				
+   }else if($category->searchtype_id == 2){	
+	   $communityFinderArr[$parentID] = $parentName;
+   }			
+   ?>
+@endforeach	 
+<?php 
+   $businessFinderJsonEncoded  = json_encode((object)$businessFinderArr);	
+   $communityFinderJsonEncoded = json_encode((object)$communityFinderArr);
+   //============================
+   $marketplaceArr = array();
+   ?>
+@foreach ($marketplaces as $category)	
+<?php 
+   $marketID = $category->id;
+   $marketplaceArr[$marketID] = $category->market_category;			
+   ?>
+@endforeach	 
+<?php 
+   $marketplaceJsonEncoded = json_encode($marketplaceArr);
+   ?>
+
+<!-- xxxxxxxxxxxxxxxxxxxxxxxxx Angular controller xxxxxxxxxxxxxxxxxxxxxxxxx -->    
+
+<script>
+   var app = angular.module('myApp', []);  
+   
+   app.config(function($interpolateProvider) {
+		$interpolateProvider.startSymbol('%%');
+		$interpolateProvider.endSymbol('%%');
+   });
+   
+   app.controller('myCtrl', function($scope) {  
+	   $scope.cities          = <?php echo $citiesJsonEncoded ?>;	
+	   $scope.searchType      = <?php echo $searchtypesJsonEncoded ?>;		
+	   $scope.businessFinder  = <?php echo $businessFinderJsonEncoded ?>;
+	   $scope.communityFinder = <?php echo $communityFinderJsonEncoded ?>;
+	   $scope.marketplace     = <?php echo $marketplaceJsonEncoded;?>;
+	   
+	   var oldInternationalCountryID = ("{{ old('internationalCountryID') }}".split(":"))[1];
+	   var oldInternationalCityID    = ("{{ old('internationalCityID') }}".split(":"))[1];
+	   var oldUKCItyID = ("{{ old('UKCItyID') }}".split(":"))[1];
+	   var oldSearchType = ("{{ old('searchType') }}".split(":"))[1];
+	   var oldBfParent = ("{{ old('bfParent') }}".split(":"))[1];
+	   var oldBfChild  = ("{{ old('bfChild') }}".split(":"))[1];
+	   var oldComParent  = ("{{ old('comParent') }}".split(":"))[1];
+	   var oldMarketParent  = ("{{ old('marketParent') }}".split(":"))[1];
+	   
+	   $scope.master = {
+		   region:"{{ old('region') }}", 
+		   selectedcountry:oldInternationalCountryID,
+		   selectLocationInt:oldInternationalCityID,
+		   selectLocationUK:oldUKCItyID,
+		   searchTypeSelected:oldSearchType,
+		   bfParent:oldBfParent,
+		   bfChild:oldBfChild,
+		   comParent:oldComParent,
+		   oldMarketParent:oldMarketParent
+	   };
+	   $scope.reset = function() {
+		   $scope.user = angular.copy($scope.master);
+	   };
+	   $scope.reset();
+   });
+</script>
 @endsection
