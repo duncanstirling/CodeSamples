dde696e63a350913453efb029e331a8108e4dda2
unknown <dgstirling@yahoo.co.uk>
2021-07-25 12:14:17 +0100

    added QR code generator

---
+++ Laravel8_QR_Code_Generator/code/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QrCodeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/generate-qrcode', [QrCodeController::class, 'index']);
